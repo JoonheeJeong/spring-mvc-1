853a3e3b4f8b542ededc103858dfb12752bd5d10
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-11 13:39:49 +0900

    implement #2 - to save member only with servlet

---
+++ servlet/src/main/java/inflearn/kimyounghan/mvc1/servlet/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,44 @@
+package inflearn.kimyounghan.mvc1.servlet.web.servlet;
+
+import inflearn.kimyounghan.mvc1.servlet.domain.member.Member;
+import inflearn.kimyounghan.mvc1.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+    protected MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+
+        String username = req.getParameter("username");
+        Integer age = Integer.valueOf(req.getParameter("age"));
+        Member member = memberRepository.save(new Member(username, age));
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        resp.getWriter().write(
+                "<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                " <li>id="+ member.getId() + "</li>\n" +
+                " <li>username=" + member.getUsername() + "</li>\n" +
+                " <li>age=" + member.getAge() + "</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+}
