e9db73a38d6b9a87a09a0bcebd442eb3921bab97
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-12 13:37:20 +0900

    add #6 - render(model, req, resp) in MyView

--- servlet/src/main/java/inflearn/kimyounghan/mvc1/servlet/web/frontcontroller/MyView.java
+++ servlet/src/main/java/inflearn/kimyounghan/mvc1/servlet/web/frontcontroller/MyView.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -16,5 +17,15 @@ public class MyView {
     public void render(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher(viewPath)
                 .forward(req, resp);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        copyAttributes(model, req);
+        req.getRequestDispatcher(viewPath)
+                .forward(req, resp);
+    }
+
+    private void copyAttributes(Map<String, Object> model, HttpServletRequest req) {
+        model.forEach(req::setAttribute);
     }
 }
