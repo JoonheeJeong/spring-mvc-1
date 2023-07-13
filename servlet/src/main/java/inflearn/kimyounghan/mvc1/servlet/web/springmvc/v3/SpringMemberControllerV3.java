93fb6ce6071d85496ed90c91ed8a1d1ada1dba6b
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-13 21:53:04 +0900

    modify #14 - RequestMapping to either GetMapping or PostMapping

--- servlet/src/main/java/inflearn/kimyounghan/mvc1/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ servlet/src/main/java/inflearn/kimyounghan/mvc1/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,8 +4,9 @@ import inflearn.kimyounghan.mvc1.servlet.domain.member.Member;
 import inflearn.kimyounghan.mvc1.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -16,12 +17,12 @@ public class SpringMemberControllerV3 {
 
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String getNewForm() {
         return "new-form";
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
     public String save(
             @RequestParam("username") String username,
             @RequestParam("age") Integer age,
@@ -32,7 +33,7 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String getList(Model model) {
         List<Member> members = memberRepository.findAll();
         model.addAttribute("members", members);
