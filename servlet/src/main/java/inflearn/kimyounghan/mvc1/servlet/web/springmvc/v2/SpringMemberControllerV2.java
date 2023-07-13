307a787d55f3e8370f496af7239ecc92c3bdd933
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-13 21:32:51 +0900

    implement #13 - spring mvc v2 - Controller integration
    통합된 컨트롤러 버전입니다.

---
+++ servlet/src/main/java/inflearn/kimyounghan/mvc1/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,44 @@
+package inflearn.kimyounghan.mvc1.servlet.web.springmvc.v2;
+
+import inflearn.kimyounghan.mvc1.servlet.domain.member.Member;
+import inflearn.kimyounghan.mvc1.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class SpringMemberControllerV2 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v2/members/new-form")
+    public ModelAndView getNewForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/springmvc/v2/members/save")
+    public ModelAndView save(HttpServletRequest req) {
+        String username = req.getParameter("username");
+        Integer age = Integer.valueOf(req.getParameter("age"));
+
+        Member member = memberRepository.save(new Member(username, age));
+
+        ModelAndView mav = new ModelAndView("save-result");
+        mav.addObject("member", member);
+
+        return mav;
+    }
+
+    @RequestMapping("/springmvc/v2/members")
+    public ModelAndView getList() {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mav = new ModelAndView("members");
+        mav.addObject("members", members);
+
+        return mav;
+    }
+}
